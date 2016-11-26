2b001a01186c914527e8750f8664a6dcb80ddbe9
JeremyKulcsar <jeremy.kulcsar@student.ecp.fr>
2016-11-26 23:26:30 +0100

    Add files via upload

---
+++ src/cellmenu.js
@@ -0,0 +1,54 @@
+//import React, { Component } from 'react';
+//import ReactDOM from 'react-dom';
+
+class Cell extends Component {
+  constructor() {
+    super();
+    this.state = { value: null };
+  }
+
+  render() {
+    return (
+      <button className="cell" onClick={() => this.props.onClick()}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Grid extends Component {
+  constructor() {
+    super();
+    this.state = { cells: Array(9).fill(null) };
+  }
+
+  handleClick(i) {
+  const cells = this.state.cells.slice();
+  cells[i] = 'X';
+  this.setState({cells: cells});
+  }
+
+  renderCell(i) {
+  return <Cell value={this.state.cells[i]} onClick={() => this.handleClick(i)} />;
+  }
+
+  render() {
+    return (
+      <div className="grid-row">
+        {this.renderCell(0)}
+      </div>
+    );
+  }
+}
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="scrollbar" id="style-1">
+        <Grid />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Menu />, document.querySelector('.containermenu'));
