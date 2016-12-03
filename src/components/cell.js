44da71a693e53f2c0faf368afa1335c68cbfc2e4
JeremyKulcsar <jeremy.kulcsar@student.ecp.fr>
2016-12-03 17:49:30 +0100

    Add files via upload

---
+++ src/components/cell.js
@@ -0,0 +1,11 @@
+/*import React, { Component } from 'react';
+
+export default class Cell extends Component {
+  render() {
+    return (
+      <button className="cell" onClick={() => props.onClick()}>
+        {props.value}
+      </button>
+    );
+  }
+}
